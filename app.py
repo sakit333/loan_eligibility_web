14bbe2097349fdd821540e534cd9059b47d8d819
sakit333 <sak528264@gmail.com>
2025-02-20 14:52:29 +0530

    did for global host

--- app.py
+++ app.py
@@ -24,4 +24,5 @@ def predict():
     return render_template('result.html', prediction=result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
